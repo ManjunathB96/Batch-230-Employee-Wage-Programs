93dc9b268c2c8e9b1eea3b86817da4a78b77135d
Manju31051996 <bbelagavi6@gmail.com>
2022-12-28 00:09:30 +0530

    [Manjunath S B] Day-3 ->Employee Wage Computation :Display Welcome message

---
+++ src/com/day_3_employee_wage_computation/Employee.java
@@ -0,0 +1,7 @@
+package com.day_3_employee_wage_computation;
+
+public class Employee {
+    public static void main(String[] args) {
+        System.out.println("Welcome to employee wage computation");
+    }
+}
